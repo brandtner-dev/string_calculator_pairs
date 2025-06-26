fb6fa191ad9a3ae72dfceb59963276b5f696b902
YusufKhj <yusof.khouja@gmail.com>
2025-06-26 16:30:44 +0200

    Implemented Todo 3

--- src/string_calculator.js
+++ src/string_calculator.js
@@ -1,5 +1,11 @@
 function Electrongame(arr) {
     let sum = 0;
-   
+    for (let num of arr) {
+        if (num === 3) {
+            sum += 2;
+        } else if (num === 5) {
+            sum += 4;
+        }
+    }
     return sum;
 }
