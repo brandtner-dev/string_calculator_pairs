a2a0d11f976066a40cfeafbd39c3516c6a023b15
if24b245 <if24b245@technikum-wien.at>
2025-06-26 16:09:52 +0200

    Created todo list

--- spec/string_calculator_spec.js
+++ spec/string_calculator_spec.js
@@ -1,9 +1,1 @@
-describe('String Calculator', function() {
-	beforeEach(function() {
-		calculator = new StringCalculator();
-	});
 
-	it('should return 0 for an empty string', function() {
-		expect(calculator.add('')).toEqual(0);
-	});
-});
