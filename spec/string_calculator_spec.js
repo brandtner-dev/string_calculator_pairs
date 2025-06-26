1aa1e29217583140f8d422faed330aa45a9b65b8
YusufKhj <yusof.khouja@gmail.com>
2025-06-26 16:26:10 +0200

    Implemented Test for Todo 2

--- spec/string_calculator_spec.js
+++ spec/string_calculator_spec.js
@@ -3,4 +3,10 @@ describe("Electron Game", function() {
         const result = Electrongame([]);
         expect(result).toBe(0);
     });
+    describe("Electron Game", function() {
+    it("should return 0 when the array contains no 3 or 5", function() {
+        const result = Electrongame([1, 2, 4, 6]);
+        expect(result).toBe(0);
+    });
+});
 });
