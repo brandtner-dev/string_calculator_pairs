1b9661d16bc86912eca31239ac9c369ba9f6f5f2
if24b245 <if24b245@technikum-wien.at>
2025-06-26 16:11:30 +0200

    Test Case for Todo 1

--- spec/string_calculator_spec.js
+++ spec/string_calculator_spec.js
@@ -1,1 +1,6 @@
-
+describe("Electron Game", function() {
+    it("should return 0 for an empty array", function() {
+        const result = Electrongame([]);
+        expect(result).toBe(0);
+    });
+});
